e5777214565df49d0fb976b90c7a32b3e28f0bc6
jorgeeduardovl14-sudo <jorgeeduardovl14@gmail.com>
2026-08-17 10:22:15 -0600

    Create youtube-config.js

---
+++ youtube-config.js
@@ -0,0 +1,4 @@
+window.JV_YOUTUBE_CONFIG = {
+  clientId: "599378395766-ejediq6r2a6n1f2l1rgenje4qu6n7umt.apps.googleusercontent.com",
+  scope: "https://www.googleapis.com/auth/youtube.force-ssl"
+};
